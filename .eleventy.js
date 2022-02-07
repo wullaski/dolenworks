bafbbf044a01fd90d8046df6b898ab22178d79d3
John Woolschlager <wullaski@gmail.com>
2022-02-06 21:58:22 -0500

    Adds resume

--- .eleventy.js
+++ .eleventy.js
@@ -10,6 +10,7 @@ module.exports = function(eleventyConfig) {
   // Copy the `img` and `css` folders to the output
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("documents");
 
   // Add plugins
   eleventyConfig.addPlugin(pluginRss);
